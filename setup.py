2196c67f871dd0e34a4efc9fba383d68aa5d1b1d
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 08:46:49 -0800

    Add uloc_keytype

--- setup.py
+++ setup.py
@@ -164,6 +164,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'rbbidata.cpp',
         'ustrfmt.cpp',
         'ucharstrie.cpp',
+        'uloc_keytype.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
