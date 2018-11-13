86ef580f0b4041c1117cbcd3293f7f94a7782f9c
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 07:16:37 -0800

    Add stringtriebuilder

--- setup.py
+++ setup.py
@@ -150,6 +150,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'servls.cpp',
         'servlk.cpp',
         'servslkf.cpp',
+        'stringtriebuilder.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
