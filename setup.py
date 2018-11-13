5a5d976876c7208e165dad8d118a61049a0c7903
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 07:26:35 -0800

    Add strenum

--- setup.py
+++ setup.py
@@ -152,6 +152,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'servslkf.cpp',
         'stringtriebuilder.cpp',
         'uvector.cpp',
+        'ustrenum.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
