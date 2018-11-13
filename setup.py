11301e40b933faf4be342b74026b08ae3f343c39
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 08:53:32 -0800

    Add uhash

--- setup.py
+++ setup.py
@@ -165,6 +165,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'ustrfmt.cpp',
         'ucharstrie.cpp',
         'uloc_keytype.cpp',
+        'uhash.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
