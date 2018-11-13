0ac1306387e08fa4f8f66dfde4ac6e92de4679d1
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 06:28:25 -0800

    Add uobject.cpp

--- setup.py
+++ setup.py
@@ -140,7 +140,8 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'ubrk.cpp',
         'brkiter.cpp',
         'filteredbrk.cpp',
-        'ucharstriebuilder.cpp']
+        'ucharstriebuilder.cpp',
+        'uobject.cpp']
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
     # for root, dirnames, filenames in list(os.walk(ICU)):
