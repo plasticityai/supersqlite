40bb3a9df3311242934ceb77c39499a6d5b499c1
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 09:32:53 -0800

    Add uresbund.cpp

--- setup.py
+++ setup.py
@@ -171,6 +171,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'dictionarydata.cpp',
         'udataswp.cpp',
         'uinvchar.cpp',
+        'uresbund.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
