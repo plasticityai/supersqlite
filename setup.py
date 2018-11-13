39ad3a1ffaff769fec599ab7d1a6b6e2ab413668
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 08:34:00 -0800

    Add ustrfmt

--- setup.py
+++ setup.py
@@ -162,6 +162,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'umath.cpp',
         'charstr.cpp',
         'rbbidata.cpp',
+        'ustrfmt.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
