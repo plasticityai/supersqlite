8053d87a9c43bfd5ce4eb64653ae06494fc76b60
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 07:52:44 -0800

    Add rbbi.cpp

--- setup.py
+++ setup.py
@@ -156,6 +156,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'uenum.cpp',
         'unistr.cpp',
         'appendable.cpp',
+        'rbbi.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
