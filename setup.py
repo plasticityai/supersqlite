8c3d3053514357a6b2a2e82c3e3f28b4ee0a48ff
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 09:39:53 -0800

    uresdata

--- setup.py
+++ setup.py
@@ -172,6 +172,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'udataswp.cpp',
         'uinvchar.cpp',
         'uresbund.cpp',
+        'uresdata.cpp',
         ]
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
