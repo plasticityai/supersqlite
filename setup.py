845e65d4c3dba56a5aa0e9114f105b11f6cd9cf9
Ajay Patel <patel.ajay285@gmail.com>
2018-11-13 06:34:30 -0800

    Add resbund.cpp

--- setup.py
+++ setup.py
@@ -141,7 +141,8 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
         'brkiter.cpp',
         'filteredbrk.cpp',
         'ucharstriebuilder.cpp',
-        'uobject.cpp']
+        'uobject.cpp',
+        'resbund.cpp']
     # icu_skip = ['unifiedcache.cpp', 'uresdata.cpp', 'usprep.cpp',
     #             'ucnv_u7.cpp', 'ucnv2022.cpp']
     # for root, dirnames, filenames in list(os.walk(ICU)):
