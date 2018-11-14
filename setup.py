68f01aff540885ae53567580aa0decf26e4dc76a
Ajay Patel <patel.ajay285@gmail.com>
2018-11-14 01:23:28 -0800

    Set UCONFIG_NO_REGULAR_EXPRESSION

--- setup.py
+++ setup.py
@@ -277,6 +277,7 @@ def get_modules(THIRD_PARTY, INTERNAL, PROJ_PATH,
             # ifndef PLASTICITY_SUPERSQLITE_ICU_CPP
             # define PLASTICITY_SUPERSQLITE_ICU_CPP 1
 
+            #define UCONFIG_NO_REGULAR_EXPRESSIONS 0
             #define U_DISABLE_RENAMING 1
             #define U_COMMON_IMPLEMENTATION
             #define U_COMBINED_IMPLEMENTATION
